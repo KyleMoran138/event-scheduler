3bd545336fe6946842490e7173cde5da34bd636d
KyleMoran138 <kylemoran.data@gmail.com>
2021-12-08 08:34:53 -0500

    add scheduledEvent model

---
+++ src/models/ScheduledEvent.model.ts
@@ -0,0 +1,24 @@
+export default interface IScheduledEvent {
+  _id: string;
+  ownerId: string;
+  name: string;
+  cron: string;
+  requestUrl: string;
+  reportUrl: string;
+  active: boolean;
+  deleted: boolean;
+  runOnInit: boolean;
+  requestData: IRequestData;
+  reportRequestData: IRequestData;  
+}
+
+export interface IRequestData {
+  data?: unknown;
+  headers?: HeadersInit;
+}
+
+export const ScheduledEvent = (scheduledEventData: IScheduledEvent): IScheduledEvent => ({
+  ...scheduledEventData,
+  requestData: {...scheduledEventData.requestData},
+  reportRequestData: {...scheduledEventData.reportRequestData}
+})
