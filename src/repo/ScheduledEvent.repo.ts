237d1f1f2de0fff3991d9f705408477e2c1fa58d
KyleMoran138 <kylemoran.data@gmail.com>
2021-12-18 12:40:25 -0500

    remove overrides of repo methods since generic class should be fine

--- src/repo/ScheduledEvent.repo.ts
+++ src/repo/ScheduledEvent.repo.ts
@@ -2,19 +2,4 @@ import Repo from './Repo';
 import IScheduledEvent from '../models/ScheduledEvent.model';
 
 export default class ScheduledEventRepo extends Repo<IScheduledEvent> {
-  public createRecord = async (newData: Partial<IScheduledEvent>): Promise<IScheduledEvent | null> => {
-    return null;
-  };
-  public getRecord = async (_id: string): Promise<IScheduledEvent | null> => {
-    return null;
-  };
-  public getAllRecords = async (): Promise<IScheduledEvent[] | null> => {
-    return null;
-  };
-  public updateRecord = async (_id: string, newData: Partial<IScheduledEvent>): Promise<IScheduledEvent | null> => {
-    return null;
-  };
-  public deleteRecord = async (_id: string): Promise<boolean> => {
-    return false;
-  };
 } 
