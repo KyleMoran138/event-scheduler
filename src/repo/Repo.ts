f7111135e18a0164ad9d0499d0b7b8c3d0f179a1
KyleMoran138 <kylemoran.data@gmail.com>
2021-12-19 19:12:01 -0500

    remove null from returns. if it fails it should throw

--- src/repo/Repo.ts
+++ src/repo/Repo.ts
@@ -4,19 +4,19 @@ export default class Repo<T extends IModel> {
 
   protected directCallError = (methodName: string) => `REPO CLASS METHOD (${methodName}) DIRECTLY CALLED, YOU DID NOT MEAN TO DO THIS`;
 
-  public createRecord = async (newData: Partial<T>): Promise<T | null> => {
+  public createRecord = async (newData: Partial<T>): Promise<T> => {
     throw new Error(this.directCallError('createRecord'));
   }
 
-  public getRecord = async (_id: string): Promise<T | null> => {
+  public getRecord = async (_id: string): Promise<T> => {
     throw new Error(this.directCallError('getRecord'));
   }
 
-  public getAllRecords = async ({...args}): Promise<T[] | null> => {
+  public getAllRecords = async ({...args}): Promise<T[]> => {
     throw new Error(this.directCallError('getAllRecords'));
   }
 
-  public updateRecord = async (_id: string, newData: Partial<T>): Promise<T | null> => {
+  public updateRecord = async (_id: string, newData: Partial<T>): Promise<T> => {
     throw new Error(this.directCallError('updateRecord'));
   }
 
